e2070ad480a5e4266eb7e3edf1d0a3814628d486
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-13 12:35:49 +0200

    minimumWindowSubstring

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import slidingWindow.maxSumOfContiguousSubArrayK;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] $args) {
-        String input = "ppqp";
-        String pattern = "pq";
-        List<Integer> res = maxSumOfContiguousSubArrayK.findAllAnagramsOfPatternWithinString(input, pattern);
+        String input = "adcad";
+        String pattern = "abc";
+        String res = maxSumOfContiguousSubArrayK.minimumWindowSubstring(input, pattern);
 
         System.out.println(res);
     }
