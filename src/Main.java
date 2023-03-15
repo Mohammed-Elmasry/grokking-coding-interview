026dcd83fbab040b1e5a67fc8c5fb62116e093f2
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-15 13:02:55 +0200

    solve problem 2 in exercises

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] $args) {
-        int[] input = {2, 1, 5, 1, 3, 2};
-        int k = 3;
+        // problem 1
+//        int[] input = {2, 1, 5, 1, 3, 2};
+//        int k = 3;
 
-        int res = Exercises.maxSubarrayOfSizeK(input, k);
+//        int res = Exercises.maxSubarrayOfSizeK(input, k);
 
+
+        // problem 2
+        int[] input = {3, 4, 1, 1, 6};
+        int sum = 8;
+
+        int res = Exercises.smallestSubarrayWithGivenSum(input, sum);
         System.out.println(res);
     }
 }
--- src/slidingWindow/Exercises.java
+++ src/slidingWindow/Exercises.java
@@ -16,4 +16,21 @@ public class Exercises {
         }
         return bestSum;
     }
+
+    public static int smallestSubarrayWithGivenSum(int[] input, int sum) {
+        int bestLength = Integer.MAX_VALUE;
+        int windowStart = 0;
+        int currentSum = 0;
+
+        for (int windowEnd = 0; windowEnd < input.length; windowEnd++) {
+            currentSum += input[windowEnd];
+
+            while (currentSum >= sum) {
+                bestLength = Math.min(bestLength, windowEnd - windowStart + 1);
+                currentSum -= input[windowStart++];
+            }
+        }
+
+        return bestLength;
+    }
 }
