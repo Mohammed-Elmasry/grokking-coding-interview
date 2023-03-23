3f49ae61b1afc46258c76f781a9458b9b838df75
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-23 13:34:38 +0200

    redefine tripleSumToZero with two pointers

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 import twoPointers.Exercises;
-import twoPointers.TwoPointers;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] $args) {
-        int[] nums = {-3, 0, 1, 2, -1, 1, -2};
-        List<List<Integer>> res = Exercises.tripleSumToZero(nums);
+        int[] nums = {-5, 2, -1, -2, 3};
+        List<List<Integer>> res = Exercises.tripleSumToZeroWithTwoPointers(nums);
 
         System.out.println(res);
 
--- src/twoPointers/Exercises.java
+++ src/twoPointers/Exercises.java
@@ -95,4 +95,33 @@ public class Exercises {
         }
         return new ArrayList<>(triplets);
     }
+
+    public static List<List<Integer>> tripleSumToZeroWithTwoPointers(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> triplets = new ArrayList<>();
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            int left = i + 1;
+            int right = arr.length - 1;
+
+            while (left < right) {
+                int total = arr[i] + arr[left] + arr[right];
+                if (total == 0) {
+                    triplets.add(Arrays.asList(arr[i], arr[left], arr[right]));
+                    left++;
+                    right--;
+                    while (left < right && arr[left] == arr[left - 1]) left++;
+                    while (left < right && arr[right] == arr[right + 1]) right--;
+                } else if (total < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return triplets;
+    }
 }
