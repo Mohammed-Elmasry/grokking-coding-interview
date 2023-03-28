edf1bc9bed701569b02bff1b4723208cc0dc6e68
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-03-28 14:14:36 +0200

    define Floyds algorithm [Tortoise and Hare]

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] $args) {
-//        Node head = new Node(1);
-//        head.next = new Node(2);
-//        head.next.next = new Node(3);
-//        head.next.next.next = new Node(4);
-//        head.next.next.next.next = new Node(5);
-//        head.next.next.next.next.next = new Node(6);
-//
-//        head.next.next.next.next.next = head.next.next;
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
 
-        Node head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
+        head.next.next.next.next.next = head;
 
-        boolean res = FastAndSlowPointers.hasCycle(head);
+//        Node head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
 
-        System.out.println(res);
+        Node res = FastAndSlowPointers.getCycleStart(head);
+
+        if (res != null) {
+            System.out.println(res.val);
+        } else {
+            System.out.println("It is null");
+        }
     }
 }
--- src/fastAndSlowPointers/FastAndSlowPointers.java
+++ src/fastAndSlowPointers/FastAndSlowPointers.java
@@ -15,4 +15,47 @@ public class FastAndSlowPointers {
 
         return false;
     }
+
+    public static int getCycleLength(Node head) {
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) { // cycle found
+                Node current = slow.next;
+                int cycleLength = 1;
+                while (current != slow) {
+                    current = current.next;
+                    cycleLength++;
+                }
+
+                return cycleLength;
+            }
+        }
+
+        return 0;
+    }
+
+    public static Node getCycleStart(Node head) {
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) { // cycle found
+                Node newSlow = head;
+
+                while (newSlow != slow) {
+                    slow = slow.next;
+                    newSlow = newSlow.next;
+                }
+                return newSlow;
+            }
+        }
+        return null;
+    }
 }
