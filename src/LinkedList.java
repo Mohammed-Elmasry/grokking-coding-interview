e0e59406205be72645b73b9dc5da27192fffa2da
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-04-08 15:12:02 +0200

    define reverseLinkedList()

---
+++ src/LinkedList.java
@@ -0,0 +1,26 @@
+import fastAndSlowPointers.Node;
+
+public class LinkedList {
+
+    public static Node reverseLinkedList(Node head) {
+        Node prev = null;
+
+        while (head != null) {
+            Node next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
+    public static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.print("Null");
+    }
+
+}
